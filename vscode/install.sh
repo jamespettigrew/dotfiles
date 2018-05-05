34e881e76c6fad883bb64d68125f3784f4b73c18
James Pettigrew <james@innovum.com.au>
2018-05-05 18:31:18 +1000

    Add Nord colour theme

--- vscode/install.sh
+++ vscode/install.sh
@@ -2,3 +2,4 @@ code --install-extension Ionide.ionide-fsharp
 code --install-extension ms-python.python
 code --install-extension rust-lang.rust
 code --install-extension vscodevim.vim
+code --install-extension arcticicestudio.nord-visual-studio-code
