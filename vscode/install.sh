985db60f0b7e9c072072732359bec7b0febe8cab
James Pettigrew <james@innovum.com.au>
2018-11-20 15:24:54 +1100

    Install relative line numbers extension for VS Code

--- vscode/install.sh
+++ vscode/install.sh
@@ -3,3 +3,4 @@ code --install-extension ms-python.python
 code --install-extension rust-lang.rust
 code --install-extension vscodevim.vim
 code --install-extension arcticicestudio.nord-visual-studio-code
+code --install-extension extr0py.vscode-relative-line-numbers
